e572c4a06c28129b17c6847817378534704524a8
Stanislav Chvila <schvila@gmail.com>
2021-06-20 18:51:45 +0200

    input form before saving

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,16 +1,12 @@
 import React from "react";
-import "./ExpenseForm.css";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
   return (
-    <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" />
-        </div>
+      <div className="new-expense">
+        <ExpenseForm />
       </div>
-    </form>
   );
 };
 export default NewExpense;
