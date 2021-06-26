98c637e6ad2f63cabe5a2857c79accbcd026755a
Standa Chvila <schvila@gmail.com>
2021-06-26 07:58:16 +0200

    add controlled item - filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
   const clickHandler = () => {
-    setTitle("Updatet");
-    console.log(title); //new title not here, it's just scheduled to change
+    setTitle('Updated!');
+    console.log(title);
   };
+
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
+      <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>change title</button>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-};
+}
 
 export default ExpenseItem;
