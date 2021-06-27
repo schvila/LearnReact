d9482211901acfc6238ce0510d7783461a7ec6e5
Standa Chvila <schvila@gmail.com>
2021-06-27 08:20:30 +0200

    Conditional new item form with Cancel handler

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,38 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-// beaware of adding key prop in ExpenseItem component, not pnly for perfomanve but even state can be lost.
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  const filteredExpenses = props.items.filter(
-    (i) => i.date.getFullYear().toString() === filteredYear
-  );
-  let expenseContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />
-    ));
-  }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
